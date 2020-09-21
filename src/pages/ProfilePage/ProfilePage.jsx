0f07db91b5123c969ef805b5d9a6767b25a79507
Chloe <66706246+lex-c@users.noreply.github.com>
2020-09-21 13:33:19 -0400

    Update ProfilePage.jsx

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -3,10 +3,13 @@ import * as authService from '../../service/authService'
 import FavoriteDates from '../../Components/FavoriteDates/FavoriteDates'
 
 function ProfilePage(props) {
+        sortPhotosByDate() {
+                console.log('sorting photos')
+        }
         return ( 
             <>
             <h1>{props.user.name}'s Profile</h1>
-            <FavoriteDates user={props.user} />
+            <FavoriteDates user={props.user} sortPhotosByDate={sortPhotosByDate}/>
             </>
          );
     }
