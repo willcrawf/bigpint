5ae61d0ebb632ba4617cde24758c01f20e1978e1
Chloe <66706246+lex-c@users.noreply.github.com>
2020-09-21 19:47:29 -0400

    Update ProfilePage.jsx

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -5,13 +5,13 @@ import AddFavoriteDate from '../../Components/AddFavoriteDate/AddFavoriteDate'
 
 function ProfilePage(props) {
         function sortPhotosByDate(datePicked) {
-                return datePicked.toLocaleDateString
                 console.log(`sorting photos by date ${datePicked}`)
+                return datePicked.toLocaleDateString()
         }
         return ( 
             <>
             <h1>{props.user.name}'s Profile</h1>
-            <FavoriteDates user={props.user} sortPhotosByDate={sortPhotosByDate}/>
+            <FavoriteDates user={props.user} sortPhotosByDate={sortPhotosByDate} />
             <AddFavoriteDate user={props.user} />
             </>
 
