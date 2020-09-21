44ad2d505a2cd9c2797e4d9aef8d15ee73771c95
Chloe <66706246+lex-c@users.noreply.github.com>
2020-09-21 19:44:11 -0400

    Update FavoriteDates.jsx

--- src/Components/FavoriteDates/FavoriteDates.jsx
+++ src/Components/FavoriteDates/FavoriteDates.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import{ Menu } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 import './FavoriteDates.css'
 
 function FavoriteDates({ user, sortPhotosByDate }) {
     return (
     <>
         <h3>Favorite Dates:</h3>
-        <div id = "dateMenu">
-        <Menu color = 'blue'>
-            {user.favDates.map(favDate => 
+        <div id="dateMenu">
+        <Menu color='blue'>
+            {user.favDates.map((favDate, i) => 
                 <Menu.Item
-                    onClick={() => sortPhotosByDate(favDate)}>{favDate}
-                 </Menu.Item>
+                    onClick={() => sortPhotosByDate(favDate)} key={i}>
+                        {favDate}
+                </Menu.Item>
             )}
         </Menu>
         </div>
