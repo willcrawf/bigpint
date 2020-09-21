87451dcd08e538038906e5b2c70718defc3b4972
Chloe <66706246+lex-c@users.noreply.github.com>
2020-09-21 19:25:17 -0400

    Delete favoriteDate.js

--- models/favoriteDate.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema
-
-const favoriteDateSchema = new Schema({
-   favDate: {type: Date, required: true, default: ['09/20/2020'].toLocaleDateString()},
-   
- })
-
- module.exports = mongoose.model('FavoriteDate', favoriteDateSchema)
