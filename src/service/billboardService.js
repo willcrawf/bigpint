f1994f5a7130aa0909d71f3f4ccac9b43af8136a
Chloe <66706246+lex-c@users.noreply.github.com>
2020-09-21 19:17:00 -0400

    Update billboardService.js

--- src/service/billboardService.js
+++ src/service/billboardService.js
@@ -2,16 +2,17 @@ const axios = require("axios");
 
 export default function getTopTen(){
 axios({
-    "method":"GET",
-    "url":"https://billboard-api2.p.rapidapi.com/hot-100",
-    "headers":{
-    "content-type":"application/octet-stream",
-    "x-rapidapi-host":"billboard-api2.p.rapidapi.com",
-    "x-rapidapi-key":process.env.REACT_APP_BILLBOARD,
-    "useQueryString":true
-    },"params":{
-    "date":"2000-05-11",
-    "range":"1-10"
+    method:"GET",
+    url:"https://billboard-api2.p.rapidapi.com/hot-100",
+    headers: {
+    "content-type": "application/octet-stream",
+    "x-rapidapi-host": "billboard-api2.p.rapidapi.com",
+    "x-rapidapi-key": process.env.REACT_APP_BILLBOARD,
+    "useQueryString": true
+    },
+    params: {
+    "date": "2000-05-11",
+    "range": "1-10"
     }
     })
     .then((response)=>{
