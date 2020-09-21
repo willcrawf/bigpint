985184c9ef89c231ffce6023d668d18c7be4e341
Chloe <66706246+lex-c@users.noreply.github.com>
2020-09-21 19:51:22 -0400

    Delete favoriteDates.js

--- routes/favoriteDates.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const favoriteDatesCtrl = require('../controllers/favoriteDates')
-
-router.get('/',favoriteDatesCtrl.index)
-
-module.exports = router
